1b8a9248ae12e302ed7887bff7fb4e904982536f
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-18 15:18:48 +0700

    Regenerate account if the address is in use.

--- resources/auth.py
+++ resources/auth.py
@@ -17,6 +17,9 @@ class Auth(Resource):
     def post(self):
         # Replace abcxyz.com with appropriate host
         new_address = generate_random_string() + '@abcxyz.com'
+        while UserModel.find_by_address(new_address) is not None:
+            # There is already an account with this address
+            new_address = generate_random_string() + '@abcxyz.com'
         token = create_access_token(identity=new_address)
         new_user = UserModel(new_address, token)
         new_user.save_to_db()
