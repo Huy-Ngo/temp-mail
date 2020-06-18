73cdf02f52e4411379ac69b90a18c4542fef0bc4
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-18 14:44:50 +0700

    Implement API resources to show mails to user

--- resources/mail_server.py
+++ resources/mail_server.py
@@ -1,4 +1,9 @@
 from smtpd import SMTPServer
+from http import HTTPStatus
+
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from models import MailModel
 
 
@@ -8,7 +13,41 @@ class TempMailServer(SMTPServer):
         print('Message addressed from:', mailfrom)
         print('Message addressed to:', rcpttos)
         print('Message length:', len(data))
-        message = MailModel(mailfrom, rcpttos, data)
-        message.save_to_db()
+        for rcpt in rcpttos:
+            message = MailModel(mailfrom, rcpt, data)
+            message.save_to_db()
         all_mails = MailModel.fetch_all()
         print(*[mail.json() for mail in all_mails], sep='\n')
+
+
+class Mailbox(Resource):
+    @jwt_required
+    def get(self):
+        """Retrieve all mails from the mail box."""
+        address = get_jwt_identity()
+        mails = MailModel.fetch_by_address(address)
+        if len(address) == 0:
+            return {
+                'message': 'No mails found.',
+                'status': HTTPStatus.NOT_FOUND
+            }
+        return {
+            'mails': [mail.json() for mail in mails],
+            'status': HTTPStatus.OK
+        }
+
+
+class Mail(Resource):
+    @jwt_required
+    def get(self, _id):
+        """Display content of a mail."""
+        mail = MailModel.fetch_by_id(_id)
+        if mail is None:
+            return {
+                'message': f'No mail with id {_id} found',
+                'status': HTTPStatus.NOT_FOUND
+            }
+        return {
+            'mail': mail,
+            'status': HTTPStatus.OK
+        }
