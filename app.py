0d2010533f5ce319739625bb4f7a370d6c45cfea
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-18 15:51:32 +0700

    Init app with db and JWT

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from asyncore import loop
 
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
-from resources import TempMailServer, Mailbox
+from resources import TempMailServer, Mailbox, Auth
 
 app = Flask(__name__)
 app.config.from_json('config.json')
 
 api = Api(app)
-
-server = TempMailServer(('127.0.0.1', 1025), None)
-loop()
+jwt = JWTManager(app)
+db.init_app(app)
 
 api.add_resource(Mailbox, '/mail/')
+api.add_resource(Auth, '/auth/')
 
 
 @app.before_first_request
@@ -24,3 +25,5 @@ def create_database():
 
 if __name__ == '__main__':
     app.run()
+    server = TempMailServer(('127.0.0.1', 1025), None)
+    loop()
