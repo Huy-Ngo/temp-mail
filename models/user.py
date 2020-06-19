dfa415d2aea42de4c80920a63fe0496b7f0dc0f6
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-19 10:02:12 +0700

    Make it possible to expire, resolve #1

--- models/user.py
+++ models/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from db import db
 
 
@@ -6,6 +8,7 @@ class UserModel(db.Model):
     email_address = db.Column(db.String(254))
     token = db.Column(db.String(256))
     is_expired = db.Column(db.Boolean)
+    create_at = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __init__(self, email_address, token):
         self.email_address = email_address
@@ -13,10 +16,13 @@ class UserModel(db.Model):
         self.is_expired = False
 
     def json(self):
-        return {'id': self.id,
-                'email_address': self.email_address,
-                'token': self.token,
-                'is_expired': self.is_expired}
+        return {
+            'id': self.id,
+            'email_address': self.email_address,
+            'token': self.token,
+            'is_expired': self.is_expired,
+            'create_at': self.create_at.__str__()
+        }
 
     @classmethod
     def find_by_address(cls, address):
@@ -29,6 +35,15 @@ class UserModel(db.Model):
     @classmethod
     def fetch_all(cls):
         return cls.query.all()
+
+    def check_validity(self):
+        """Check if the token is still valid.
+
+        :return: `True` if it is still valid
+        `False` if it has expired."""
+        expiration_time = self.create_at + timedelta(minutes=15)
+        current_time = datetime.utcnow()
+        return expiration_time < current_time
 
     def save_to_db(self):
         db.session.add(self)
--- resources/mail_server.py
+++ resources/mail_server.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from models import MailModel
+from models import MailModel, UserModel
 
 
 class TempMailServer(SMTPServer):
@@ -29,14 +29,14 @@ class Mailbox(Resource):
             return {
                 'message': 'The mail account does not exists or has expired.'
             }, HTTPStatus.BAD_REQUEST
+        if not UserModel.find_by_address(address).check_validity():
+            return {'message': 'Your token has expired'}, HTTPStatus.UNAUTHORIZED
         mails = MailModel.fetch_by_address(address)
         if len(address) == 0:
             return {
                 'message': 'No mails found.',
             }, HTTPStatus.NOT_FOUND
-        return {
-            'mails': [mail.json() for mail in mails]
-        }, HTTPStatus.OK
+        return {'mails': [mail.json() for mail in mails]}, HTTPStatus.OK
 
 
 class Mail(Resource):
@@ -44,10 +44,14 @@ class Mail(Resource):
     def get(self, _id):
         """Display content of a mail."""
         mail = MailModel.fetch_by_id(_id)
+        user = get_jwt_identity()
         if mail is None:
             return {
-                'message': f'No mail with id {_id} found',
+                'message': f'No mail with id {_id} found'
             }, HTTPStatus.NOT_FOUND
-        return {
-            'mail': mail
-        }, HTTPStatus.OK
+        recipient = mail.json()['recipient']
+        if recipient != user:
+            return {
+                'message': 'You are unauthorized to read this message.'
+            }, HTTPStatus.UNAUTHORIZED
+        return {'mail': mail.json()}, HTTPStatus.OK
