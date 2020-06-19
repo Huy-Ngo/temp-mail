553525a535157dcfaf7565bed37640fc482edc8d
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-19 11:30:49 +0700

    Run mail and web server in two separate process

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from asyncore import loop
-from threading import Thread
+from multiprocessing import Process
 
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
-from resources import TempMailServer, Mailbox, Auth
+from resources import TempMailServer, Mailbox, Auth, Mail
 
 app = Flask(__name__)
 app.config.from_json('config.json')
@@ -16,6 +16,7 @@ jwt = JWTManager(app)
 db.init_app(app)
 
 api.add_resource(Mailbox, '/mail/')
+api.add_resource(Mail, '/mail/<int:_id>')
 api.add_resource(Auth, '/auth/')
 
 
@@ -31,11 +32,11 @@ def run_mail_server():
 
 
 if __name__ == '__main__':
-    app_thread = Thread(target=app.run)
-    mail_server_thread = Thread(target=run_mail_server)
+    app_proc = Process(target=app.run)
+    mail_server_proc = Process(target=run_mail_server)
 
-    app_thread.start()
-    mail_server_thread.start()
+    app_proc.start()
+    mail_server_proc.start()
 
-    app_thread.join()
-    mail_server_thread.join()
+    app_proc.join()
+    mail_server_proc.join()
