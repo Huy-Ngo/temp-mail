dc9894d8db6751ec176363747e45edefb2eac5c6
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-19 11:43:09 +0700

    Dropping mail server to resolve #2
    The mail server is supposed to be in a separate project and only call request to this API.

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from asyncore import loop
-from multiprocessing import Process
-
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
-from resources import TempMailServer, Mailbox, Auth, Mail
+from resources import Mailbox, Auth, Mail
 
 app = Flask(__name__)
 app.config.from_json('config.json')
@@ -25,18 +22,5 @@ def create_database():
     db.create_all()
 
 
-def run_mail_server():
-    server = TempMailServer(('127.0.0.1', 1025), None)
-    print('This is running.')
-    loop()
-
-
 if __name__ == '__main__':
-    app_proc = Process(target=app.run)
-    mail_server_proc = Process(target=run_mail_server)
-
-    app_proc.start()
-    mail_server_proc.start()
-
-    app_proc.join()
-    mail_server_proc.join()
+    app.run()
