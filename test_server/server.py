d07d7dbda71de42188ee1034265f28a0953c6866
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-03 15:13:37 +0700

    Edit mail server's API call

--- test_server/server.py
+++ test_server/server.py
@@ -3,14 +3,32 @@ import asyncore
 import requests
 from threading import Thread
 
+from email.parser import Parser, BytesParser
+from email.policy import default
+
 url = 'http://127.0.0.1:5000/mail/'
 
 
 def send_request(mailfrom, rcpttos, data):
+    data = data.decode("utf-8")
+    headers = Parser(policy=default).parsestr(data)
+    content = headers.get_payload()
+    if content is list:
+        text = content[0].get_payload()
+        html = content[0].get_payload()
+    else:
+        text = content
+        html = ''
+    print(headers)
     requests.post(url, {
             'sender': mailfrom,
             'recipient': rcpttos[0],
-            'message': data
+            'mail_from': headers['from'],
+            'rcpt_to': headers['to'],
+            'date': headers['date'],
+            'subject': headers['subject'],
+            'text': text,
+            'html': html
         })
 
 
