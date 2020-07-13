139cac113ed60fb4fc10ef8f3870653bc4a8e810
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-13 13:00:09 +0700

    Add config from config.json

--- resources/auth.py
+++ resources/auth.py
@@ -1,11 +1,16 @@
 from string import digits, ascii_lowercase
 from random import choice
 from http import HTTPStatus
+from json import load
 
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token
 
 from models import UserModel
+
+with open('config.json', 'r') as f:
+    data = load(f)
+    host = data['HOST']
 
 
 def generate_random_string():
@@ -28,10 +33,10 @@ class Auth(Resource):
         }, HTTPStatus.OK
 
     def post(self):
-        new_address = generate_random_string() + '@{host}'
+        new_address = generate_random_string() + f'@{host}'
         while UserModel.find_by_address(new_address) is not None:
             # There is already an account with this address
-            new_address = generate_random_string() + '@{host}'
+            new_address = generate_random_string() + f'@{host}'
         token = create_access_token(identity=new_address)
         new_user = UserModel(new_address, token)
         new_user.save_to_db()
