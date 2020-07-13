23895a5ef6f1cd7c86a6044086b946a1d0009147
Huy Ngo <duchuy29092000@gmail.com>
2020-07-13 11:56:24 +0700

    Simplify routes

--- blueprints.py
+++ blueprints.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, flash, request, render_template, redirect, url_for
 from test_server.client import new_address, send_mail, receive_mail
 
-bp = Blueprint('gui', __name__, url_prefix='/gui')
+bp = Blueprint('gui', __name__, url_prefix='')
 
 
-@bp.route('/auth', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def auth():
     if request.method == 'POST':
         address, token = new_address()
@@ -12,20 +12,20 @@ def auth():
     return render_template('views/auth.html')
 
 
-@bp.route('/mail/<address>/<token>')
+@bp.route('/<address>/<token>')
 def mailbox(address, token):
     url_for('mailbox', address=address, token=token)
     all_mails = receive_mail(token)
     if 'mails' not in all_mails:
-        return render_template('views/error.html', message=all_mails['msg'])
+        return render_template('views/error.html', message=all_mails['message'])
     return render_template('views/mailbox.html', address=address, token=token, mails=all_mails['mails'])
 
 
-@bp.route('/mail/<address>/<token>/<int:_id>')
+@bp.route('/<address>/<token>/<int:_id>')
 def mail(address, token, _id):
     all_mails = receive_mail(token)
     if 'mails' not in all_mails:
-        return render_template('views/error.html', message=all_mails['msg'])
+        return render_template('views/error.html', message=all_mails['message'])
     for email in all_mails['mails']:
         if email['id'] == _id and email['recipient'] == address:
             return render_template('views/mail.html', mail=email)
