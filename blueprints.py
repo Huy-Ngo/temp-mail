c74918bd6e5ec9a4909357096b0a5ab538f60d8a
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-15 11:08:07 +0700

    Reverse the order of the mail, resolve #20

--- blueprints.py
+++ blueprints.py
@@ -26,7 +26,9 @@ def mailbox(address, token):
                     headers={'Authorization': f'Bearer {token}'}).json()
     if 'mails' not in all_mails:
         return render_template('views/error.html', message=all_mails['msg'])
-    return render_template('views/mailbox.html', address=address, token=token, mails=all_mails['mails'])
+    mails = all_mails['mails']
+    mails = sorted(mails, reverse=True, key=lambda m: m['id'])
+    return render_template('views/mailbox.html', address=address, token=token, mails=mails)
 
 
 @bp.route('/mail/<address>/<token>/<int:_id>')
