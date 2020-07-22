8e8b56383cba19cee21f8ccda6d1be4a727ae759
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-22 16:52:39 +0700

    Write test for parsing tree in case of pure text

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from pytest import fixture
 from json import load
 
-with open('data.json', 'r') as f:
+with open('tests/data.json', 'r') as f:
     data = load(f)
 
 
@@ -9,7 +9,21 @@ with open('data.json', 'r') as f:
 def raw_text_mail():
     """MIME data received from a text mail sent from Gmail."""
     global data
-    return data['raw-text-mime'].encode('utf-8')
+    return data['text-mail']['raw']
+
+
+@fixture
+def expected_text_html():
+    """Expected HTML representation of a mail (before quopri decode)."""
+    global data
+    return data['text-mail']['expected-html']
+
+
+@fixture
+def expected_text_plain():
+    """Expected plain text of a mail."""
+    global data
+    return data['text-mail']['expected-plain']
 
 
 @fixture
@@ -17,9 +31,3 @@ def raw_image_mail():
     """MIME data received from a mail with an image sent from Gmail."""
     global data
     return data['raw-image-mime']
-
-
-@fixture
-def html():
-    """HTML representation of a mail."""
-    pass
--- tests/test_smtp_server.py
+++ tests/test_smtp_server.py
@@ -1,2 +1,12 @@
+from email.parser import Parser
+from email.policy import default
 from server import parse_payload_tree
+
+
+def test_parse_payload_tree(raw_text_mail, expected_text_html, expected_text_plain):
+    """Test parse_payload_tree with text mail."""
+    mime_data = Parser(policy=default).parsestr(raw_text_mail)
+    parsed_data = parse_payload_tree(mime_data)
+    assert parsed_data['text/plain'] == expected_text_plain
+    assert parsed_data['text/html'] == expected_text_html
 
