fc8954107d3740a5836207e9444e34eff82d5ef8
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-22 15:55:42 +0700

    Add document for fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,19 @@ with open('data.json', 'r') as f:
 
 @fixture
 def raw_text_mail():
+    """MIME data received from a text mail sent from Gmail."""
     global data
-    return data['raw-text-mime']
+    return data['raw-text-mime'].encode('utf-8')
 
 
 @fixture
 def raw_image_mail():
+    """MIME data received from a mail with an image sent from Gmail."""
     global data
     return data['raw-image-mime']
 
 
 @fixture
 def html():
+    """HTML representation of a mail."""
     pass
