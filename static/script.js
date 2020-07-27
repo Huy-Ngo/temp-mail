64615ba5a81370ad89d9319c2fc9319f3f557d02
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-27 11:24:24 +0700

    Add tooltips for customized address

---
+++ static/script.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2020  Ngô Ngọc Đức Huy
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+  });
