4b72a7e10ea7641a85b027e4d16ebbcb016af503
Huy-Ngo <duchuy29092000@gmail.com>
2020-07-27 15:47:45 +0700

    Modify the database schema to include refresh_token

--- models/user.py
+++ models/user.py
@@ -9,22 +9,25 @@ class UserModel(db.Model):
     """Model for representing and storing addresses."""
     id = db.Column(db.Integer, primary_key=True)
     email_address = db.Column(db.String(254))
-    token = db.Column(db.String(256))
+    access_token = db.Column(db.String(256))
+    refresh_token = db.Column(db.String(256))
     is_expired = db.Column(db.Boolean)
     create_at = db.Column(db.DateTime, default=datetime.utcnow)
     mails = db.relationship('MailModel', backref='user', lazy=True)
 
-    def __init__(self, email_address: str, token: str):
+    def __init__(self, email_address: str, access_token: str, refresh_token: str):
         self.email_address = email_address
-        self.token = token
+        self.access_token = access_token
         self.is_expired = False
+        self.refresh_token = refresh_token
 
     def json(self):
         """Return the information of the object as a JSON"""
         return {
             'id': self.id,
             'email_address': self.email_address,
-            'token': self.token,
+            'access_token': self.access_token,
+            'refresh_token': self.refresh_token,
             'is_expired': self.is_expired,
             'create_at': self.create_at.__str__()
         }
@@ -35,7 +38,7 @@ class UserModel(db.Model):
         return cls.query.filter_by(email_address=address).first()
 
     def check_validity(self):
-        """Check if the token is still valid.
+        """Check if the access_token is still valid.
 
         :return: `True` if it is still valid
         `False` if it has expired."""
